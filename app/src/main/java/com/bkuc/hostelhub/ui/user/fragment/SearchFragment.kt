0b3072536ebede6369a7283d61aaaca7dd75506c
Nazeer Ahmad <afzalsuleman17@gmail.com>
2026-05-12 16:16:52 +0500

    done profile screen design

--- app/src/main/java/com/bkuc/hostelhub/ui/user/fragment/SearchFragment.kt
+++ app/src/main/java/com/bkuc/hostelhub/ui/user/fragment/SearchFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bkuc.hostelhub.R
 import com.bkuc.hostelhub.adapter.FindHostelAdapter
 import com.bkuc.hostelhub.databinding.FragmentSearchBinding
@@ -48,9 +49,8 @@ class SearchFragment : Fragment() {
                 // Navigate to Detail Fragment here if needed
             }
         )
-        // 2. IMPORTANT: RecyclerView ko layout manager aur adapter dena
         binding.rvFindHostels.apply {
-            layoutManager = androidx.recyclerview.widget.LinearLayoutManager(requireContext())
+            layoutManager = LinearLayoutManager(requireContext())
             adapter = hostelAdapter
         }
     }
